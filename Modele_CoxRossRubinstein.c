40ee85b0905ced13e99c454ddcba1298d5495ddb
Mekasup <bastih@hotmail.fr>
2015-05-18 11:41:43 +0200

    debut premier pricer

--- Modele_CoxRossRubinstein.c
+++ Modele_CoxRossRubinstein.c
@@ -3,3 +3,8 @@
 #include <math.h>
 #include "Modele_CoxRossRubinstein.h"
 
+double price1(int N,double rN,double hN,double bN,double s) {
+	double rep=0.0;
+	
+	return rep;
+}
--- Modele_CoxRossRubinstein.h
+++ Modele_CoxRossRubinstein.h
@@ -1,4 +1,6 @@
 #ifndef MODELE_COXROSSRUBINSTEIN
 #define MODELE_COXROSSRUBINSTEIN
 
+double price1(int N,double rN,double hN,double bN,double s);
+
 #endif
